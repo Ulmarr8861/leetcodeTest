82dbe24c253e18b48a31f03506dcc64e07611809
Ulmarr8861 <1144295551@qq.com>
2022-01-05 14:31:25 +0800

    添加leetcode文件

---
+++ test01/src/editor/cn/[10]正则表达式匹配.java
@@ -0,0 +1,112 @@
+package editor.cn;//给你一个字符串 s 和一个字符规律 p，请你来实现一个支持 '.' 和 '*' 的正则表达式匹配。
+//
+// 
+// '.' 匹配任意单个字符 
+// '*' 匹配零个或多个前面的那一个元素 
+// 
+//
+// 所谓匹配，是要涵盖 整个 字符串 s的，而不是部分字符串。 
+// 
+//
+// 示例 1： 
+//
+// 
+//输入：s = "aa" p = "a"
+//输出：false
+//解释："a" 无法匹配 "aa" 整个字符串。
+// 
+//
+// 示例 2: 
+//
+// 
+//输入：s = "aa" p = "a*"
+//输出：true
+//解释：因为 '*' 代表可以匹配零个或多个前面的那一个元素, 在这里前面的元素就是 'a'。因此，字符串 "aa" 可被视为 'a' 重复了一次。
+// 
+//
+// 示例 3： 
+//
+// 
+//输入：s = "ab" p = ".*"
+//输出：true
+//解释：".*" 表示可匹配零个或多个（'*'）任意字符（'.'）。
+// 
+//
+// 示例 4： 
+//
+// 
+//输入：s = "aab" p = "c*a*b"
+//输出：true
+//解释：因为 '*' 表示零个或多个，这里 'c' 为 0 个, 'a' 被重复一次。因此可以匹配字符串 "aab"。
+// 
+//
+// 示例 5： 
+//
+// 
+//输入：s = "mississippi" p = "mis*is*p*."
+//输出：false 
+//
+// 
+//
+// 提示： 
+//
+// 
+// 1 <= s.length <= 20 
+// 1 <= p.length <= 30 
+// s 可能为空，且只包含从 a-z 的小写字母。 
+// p 可能为空，且只包含从 a-z 的小写字母，以及字符 . 和 *。 
+// 保证每次出现字符 * 时，前面都匹配到有效的字符 
+// 
+// Related Topics 递归 字符串 动态规划 👍 2491 👎 0
+
+
+//leetcode submit region begin(Prohibit modification and deletion)
+class Solution {
+    public boolean isMatch(String s, String p) {
+        if (p == null) {
+            if (s == null) {
+                return true;
+            } else {
+                return false;
+            }
+        }
+
+        if (s == null && p.length() == 1) {
+            return false;
+        }
+
+        int m = s.length() + 1;
+        int n = p.length() + 1;
+
+        boolean[][] dp = new boolean[m][n];
+
+        dp[0][0] = true;
+
+        for (int j = 2; j < n; j++) {
+            if (p.charAt(j - 1) == '*') {
+                dp[0][j] = dp[0][j - 2];
+            }
+        }
+
+        for (int r = 1; r < m; r++) {
+            int i = r - 1;
+            for (int c = 1; c < n; c++) {
+                int j = c - 1;
+                if (s.charAt(i) == p.charAt(j) || p.charAt(j) == '.') {
+                    dp[r][c] = dp[r - 1][c - 1];
+                } else if (p.charAt(j) == '*') {
+                    if (p.charAt(j - 1) == s.charAt(i) || p.charAt(j - 1) == '.') {
+                        dp[r][c] = dp[r - 1][c] || dp[r][c - 2];
+                    } else {
+                        dp[r][c] = dp[r][c - 2];
+                    }
+                } else {
+                    dp[r][c] = false;
+                }
+
+            }
+        }
+        return dp[m - 1][n - 1];
+    }
+}
+//leetcode submit region end(Prohibit modification and deletion)
