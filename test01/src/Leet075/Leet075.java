62ff92f29c4fd677affa89248e1c4dbcadd5852e
ulmarr8861 <ulmarr8861@163.com>
2022-02-26 20:39:51 +0800

    2022/2/26

---
+++ test01/src/Leet075/Leet075.java
@@ -0,0 +1,45 @@
+package Leet075;
+
+public class Leet075 {
+    public static void main(String[] args) {
+        int[] nums = new int[]{2, 0, 2, 1, 1, 0};
+        new Leet075().sortColors(nums);
+    }
+
+    //冒泡排序
+    public void sortColors(int[] nums) {
+        int tmp = 0;
+        for (int i = 0; i < nums.length - 1; i++) {
+            for (int j = 0; j < nums.length - i - 1; j++) {
+                if (nums[j] >= nums[j + 1]) {
+                    tmp = nums[j];
+                    nums[j] = nums[j + 1];
+                    nums[j + 1] = tmp;
+                }
+            }
+        }
+    }
+
+
+    //重点方法
+    public void sortColors2(int[] nums) {
+        int n = nums.length;
+        int ptr = 0;
+        for (int i = 0; i < n; ++i) {
+            if (nums[i] == 0) {
+                int temp = nums[i];
+                nums[i] = nums[ptr];
+                nums[ptr] = temp;
+                ++ptr;
+            }
+        }
+        for (int i = ptr; i < n; ++i) {
+            if (nums[i] == 1) {
+                int temp = nums[i];
+                nums[i] = nums[ptr];
+                nums[ptr] = temp;
+                ++ptr;
+            }
+        }
+    }
+}
