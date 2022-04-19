56ae1adf6ca6c67a4a5b7c04abf71a33969822d6
ulmarr8861 <ulmarr8861@163.com>
2022-04-19 21:20:18 +0800

    feat:interview

---
+++ interview/src/素数.java
@@ -0,0 +1,27 @@
+import java.util.ArrayList;
+
+public class 素数 {
+    public ArrayList<Integer> findPrimeNumber (int m, int n) {
+        // write code here
+        ArrayList<Integer> ans = new ArrayList<>();
+        for (int i = m; i <= n; i++) {
+            if (isPrime(i)) {
+                ans.add(i);
+            }
+        }
+        return ans;
+    }
+
+    private boolean isPrime(int i) {
+        if (i <= 3) {
+            return i !=2 ;
+        }
+        for (int j = 2; j < i; j++) {
+            if (i % j == 0) {
+                return false;
+            }
+
+        }
+        return true;
+    }
+}
